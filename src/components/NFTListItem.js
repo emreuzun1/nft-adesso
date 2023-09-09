e3788c4a63ea66d2f316d8dee73958a992129ab4
Emre Uzun <uzun.emre1@outlook.com>
2023-09-09 06:39:16 +0300

    pinata gateway deleted

--- src/components/NFTListItem.js
+++ src/components/NFTListItem.js
@@ -2,7 +2,7 @@ import { Grid } from "@mui/material";
 
 const NFTListItem = ({ data }) => {
   return (
-    <Grid item xs={6} md={4} lg={2}>
+    <Grid item xs={12} md={4} lg={3} xl={2}>
       <img className="nft-img" src={data.image} alt={data.name} />
       <h4 className="nft-name">{data.name}</h4>
     </Grid>
--- src/utils/fetchFromPinata.js
+++ src/utils/fetchFromPinata.js
@@ -1,7 +1,3 @@
-const GATEAWAY_DOMAIN = "https://beige-effective-frog-364.mypinata.cloud";
-const PINATA_GATEAWAY_TOKEN =
-  "Q4UfotrXq1KU8LDUI8tzd4GhgKSINk1FgV7UKaA2rSE1xbZ_DgIFU5tA_M8u86zP";
-
 // To get the data from pinata we have to do it this way.
 export const fetchDataFromPinata = async (token) => {
   const response = await fetch(`${modifyDataWithGateawayUrl(token)}`);
@@ -10,9 +6,9 @@ export const fetchDataFromPinata = async (token) => {
   return data;
 };
 
-// We have to use ipfs/... instead of ipfs://... because of gateaway
+// We have to use ipfs/... instead of ipfs://...
 // That's why we have to remove from url
 const modifyDataWithGateawayUrl = (data) => {
   let modifiedData = data.replace(":/", "");
-  return `${GATEAWAY_DOMAIN}/${modifiedData}?pinataGatewayToken=${PINATA_GATEAWAY_TOKEN}`;
+  return `https://ipfs.io/${modifiedData}`;
 };
